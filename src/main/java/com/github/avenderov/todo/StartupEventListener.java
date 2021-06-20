263bc9be51582dd56b8ba623fc61c800ecb085e3
Alexey Venderov <alexey.venderov@getmoss.com>
2021-06-20 20:24:45 +0200

    listen to StartupEvent

--- src/main/java/com/github/avenderov/todo/StartupEventListener.java
+++ src/main/java/com/github/avenderov/todo/StartupEventListener.java
@@ -1,29 +1,36 @@
 package com.github.avenderov.todo;
 
 import io.micronaut.context.event.ApplicationEventListener;
-import io.micronaut.runtime.server.event.ServerStartupEvent;
+import io.micronaut.context.event.StartupEvent;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-public class Bootstrap implements ApplicationEventListener<ServerStartupEvent> {
+public class StartupEventListener implements ApplicationEventListener<StartupEvent> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Bootstrap.class);
+    private static final Logger LOG = LoggerFactory.getLogger(StartupEventListener.class);
 
     private final TodoRepository todoRepository;
 
     @Inject
-    public Bootstrap(TodoRepository todoRepository) {
+    public StartupEventListener(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
     @Override
-    public void onApplicationEvent(ServerStartupEvent event) {
+    public void onApplicationEvent(StartupEvent event) {
+        cleanup();
+        populate();
+    }
+
+    private void cleanup() {
         LOG.info("Cleaning up database.");
         todoRepository.findAll().forEach(todoRepository::delete);
+    }
 
+    private void populate() {
         final var completed = new TodoEntity("Completed todo", /* completed= */ true);
 
         todoRepository.save(completed);
