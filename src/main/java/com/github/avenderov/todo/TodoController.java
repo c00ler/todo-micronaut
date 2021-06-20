9a0d947dc92ec5e043c24e5e995b1439e2f284a8
Alexey Venderov <alexey.venderov@getmoss.com>
2021-06-20 18:21:07 +0200

    add the rest of controller methods

--- src/main/java/com/github/avenderov/todo/TodoController.java
+++ src/main/java/com/github/avenderov/todo/TodoController.java
@@ -2,8 +2,12 @@ package com.github.avenderov.todo;
 
 import io.micronaut.data.model.Sort;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Put;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import jakarta.inject.Inject;
@@ -29,5 +33,20 @@ public class TodoController {
         return todoRepository.findById(id)
             .map(HttpResponse::ok)
             .orElseGet(HttpResponse::notFound);
+    }
+
+    @Put
+    public void update(@Body TodoEntity entity) {
+        todoRepository.update(entity);
+    }
+
+    @Post
+    public TodoEntity create(@Body TodoEntity entity) {
+        return todoRepository.save(entity);
+    }
+
+    @Delete("/{id}")
+    public void delete(Long id) {
+        todoRepository.deleteById(id);
     }
 }
--- src/test/java/com/github/avenderov/todo/ApplicationTest.java
+++ src/test/java/com/github/avenderov/todo/ApplicationTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @MicronautTest
-class TodoMicronautTest {
+class ApplicationTest {
 
     @Inject
     EmbeddedApplication<?> application;
